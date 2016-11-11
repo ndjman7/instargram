2f7e0054c269e0c72a8db9306c68a58040e2445b
pando <ndjman7@gmail.com>
2016-11-11 15:19:54 +0900

    Make member models

--- django_app/member/models.py
+++ django_app/member/models.py
@@ -7,4 +7,65 @@ class MyUserManager(UserManager):
 
 
 class MyUser(AbstractUser):
-    pass
+    img_profile = models.ImageField(
+        upload_to='photo/profile',
+        blank=True
+    )
+
+    following_user = models.ManyToManyField(
+        'self',
+        symmetrical=False,
+        through='Relationship',
+        related_name='user_set_followers'
+    )
+
+    block_users = models.ManyToManyField(
+        'self',
+        symmetrical=False,
+        related_name='user_set_block'
+    )
+
+    def __str__(self):
+        return self.get_full_name()
+
+    def follow(self, user):
+        instance, created = Relationship.objects.get_or_create(
+            follower=self,
+            following=user
+        )
+        return instance
+
+    def unfollow(self, user):
+        Relationship.objects.filter(
+            follower=self,
+            following=user
+        ).delete()
+
+    def friends(self):
+        return self.following_user.all().filter(following_users=self)
+        # follower_people = self.user_set_followers.all()
+        # ans = [friend for friend in following_people if friend in follower_people]
+
+    def block(self, user):
+        self.block_users.add(user)
+
+    def unblock(self, user):
+        self.block_users.delete(user)
+
+    def is_friends(self, user):
+        if user in self.friends():
+            return True
+        return False
+
+
+class Relationship(models.Model):
+    following = models.ForeignKey(MyUser, related_name='user_following')
+    follower = models.ForeignKey(MyUser, related_name='user_follower')
+    created_date = models.DateTimeField(auto_now_add=True)
+
+    # class Meta:
+    #     unique_together = ('following', 'follower')
+
+    def __str__(self):
+        return "{}(follower)가 {}를 following 했다.".format(self.follower, self.following)
+
--- django_app/member/tests.py
+++ django_app/member/tests.py
@@ -1,3 +1,32 @@
-from django.test import TestCase
-
-# Create your tests here.
+# from django.test import LiveServerTestCase
+# from django.test import TestCase
+# from .models import MyUser
+#
+#
+# class FollowTest(LiveServerTestCase):
+#     def create_user(self, username, last_name, first_name):
+#         return MyUser.objects.create_user(
+#             username=username,
+#             last_name=last_name,
+#             first_name=first_name,
+#         )
+#
+#     def test_create_user(self):
+#         print('test_create_user')
+#         u1 = self.create_user('u1', '방', '민아')
+#         u2 = self.create_user('u2', '이', '한영')
+#         u3 = self.create_user('u3', '박', '보영')
+#
+#     def test_follow_user(self):
+#         u1 = self.create_user('u1', '방', '민아')
+#         u2 = self.create_user('u2', '이', '한영')
+#         u3 = self.create_user('u3', '박', '보영')
+#
+#         u2.follow(u1)
+#         u3.follow(u2)
+#         u3.follow(u1)
+#
+#         print(u2.following_users.all())
+#         print(u3.following_users.all())
+#
+#         print(u1.followers.all())
