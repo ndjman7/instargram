a3d7ee088e7d6069a9e335cdaf2480a42cbe1cab
pando <ndjman7@gmail.com>
2016-11-13 14:33:00 +0900

    Photo model

--- django_app/photo/admin.py
+++ django_app/photo/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+admin.site.register(Photo)
+admin.site.register(PhotoLike)
+admin.site.register(PhotoComment)
+admin.site.register(PhotoTag)
--- django_app/photo/models.py
+++ django_app/photo/models.py
@@ -2,17 +2,33 @@ from django.db import models
 from gram import settings
 from member.models import MyUser
 
+__all__ = [
+    'Photo',
+    'PhotoTag',
+    'PhotoLike',
+    'PhotoComment',
+]
+
 
 class Photo(models.Model):
     image = models.ImageField(upload_to='photo')
     author = models.ForeignKey(settings.AUTH_USER_MODEL)
     content = models.CharField(max_length=200)
-    tags = models.ManyToManyField('PhotoTag')
+    tags = models.ManyToManyField('PhotoTag', blank=True)
     like_users = models.ManyToManyField(
         settings.AUTH_USER_MODEL,
         through='PhotoLike',
         related_name='photo_set_like_users',
     )
+
+    def to_dict(self):
+        ret = {
+            'id':self.id,
+            'image':self.image.url,
+            'author':self.author.id,
+            'content':self.content,
+        }
+        return ret
 
 
 class PhotoTag(models.Model):
